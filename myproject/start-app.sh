e69dfaf33ec938c3560771d4e7e3a333f2644cc8
Juan Miguel Gabarron <juanmi@scrapinghub.com>
2017-04-05 12:45:14 +0200

    File to run gunicorn, make migrations to postgresql and collect statics

---
+++ myproject/start-app.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd myproject
+python manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+gunicorn --bind 0.0.0.0:8000 myproject.wsgi:application
+cd ..
