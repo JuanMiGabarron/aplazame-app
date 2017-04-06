9b05c02e202369a22af0a9dd2861910a0a852480
Juan Miguel Gabarron <juanmi@scrapinghub.com>
2017-04-06 09:31:07 +0200

    Change server configuration to port 80

--- myproject/start-app.sh
+++ myproject/start-app.sh
@@ -4,5 +4,5 @@ python manage.py migrate
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn --bind 0.0.0.0:8000 myproject.wsgi:application
+gunicorn --bind 0.0.0.0:80 myproject.wsgi:application
 cd ..
